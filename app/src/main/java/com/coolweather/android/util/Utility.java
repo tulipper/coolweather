79709feeff7dd759670f0b2b3265948798d8e46f
tulipper <1324227851@qq.com>
2018-03-30 17:25:08 +0800

    完成遍历省市县三级列表的功能“ git commit -m 完成遍历省市县三级列表的功能“

---
+++ app/src/main/java/com/coolweather/android/util/Utility.java
@@ -0,0 +1,81 @@
+package com.coolweather.android.util;
+
+import android.text.TextUtils;
+
+import com.coolweather.android.db.City;
+import com.coolweather.android.db.Country;
+import com.coolweather.android.db.Province;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * 解析和处理服务器返回的省级数据
+ */
+
+public class Utility {
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
+                JSONArray allProvince = new JSONArray(response);
+                for(int i = 0; i < allProvince.length(); i++) {
+                    JSONObject provinceObject = allProvince.getJSONObject(i);
+                    Province province = new Province();
+                    province.setProvinceName(provinceObject.getString("name"));
+                    province.setProvinceCode(provinceObject.getInt("id"));
+                    province.save();
+                }
+                return true;
+
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /**
+     * 解析和处理服务器返回的市级数据
+     */
+    public static boolean handleCityResponse(String response, int provinceId){
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCities = new JSONArray(response);
+                for (int i = 0; i < allCities.length(); i++) {
+                    JSONObject cityObject = allCities.getJSONObject(i);
+                    City city = new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
+                }
+                return true;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+    /**
+     * 解析和处理服务器返回的县级数据
+     */
+    public static boolean handleCountruResponse(String response, int cityId ) {
+        if (!TextUtils.isEmpty(response)){
+            try {
+                JSONArray allCountries = new JSONArray(response);
+                for (int i = 0; i <allCountries.length(); i++) {
+                    JSONObject countryObject = allCountries.getJSONObject(i);
+                    Country country = new Country();
+                    country.setCountryName(countryObject.getString("name"));
+                    country.setWeatherId(countryObject.getString("weather_id"));
+                    country.setCityId(cityId);
+                    country.save();
+                }
+                return true;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+}
